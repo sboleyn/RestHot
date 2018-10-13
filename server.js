d9eaf15e48e38b9237f4b03491cb7bfb524cf585
logiccosmic <logiccosmic@gmail.com>
2018-10-13 13:52:09 -0700

    LAST PHASE

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 var PORT = 9001;
 
-var tables = [{
+var tables = [{ 
     id: "fish",
-    person: "David",
+    name: "David",
     email: "foo@foo.com",
     phone: "1234567891"
 },
 {
     id: "fish2",
-    person: "David",
+    name: "David",
     email: "foo@foo.com",
     phone: "1234567891"
 }];
 var waitlist = [{
     id: "fish3",
-    person: "David",
+    name: "David",
     email: "foo@foo.com",
     phone: "1234567891"
 }];
@@ -60,8 +63,16 @@ app.get("/view", function(req,res){
     res.sendFile(path.join(__dirname, "/view.html"));
 })
 
-
-
+//
+// Make new Table
+//
+app.post("/make", function(req, res){
+    var table = req.body;
+    console.log(table);
+    tables.push(table);
+    console.log(tables);
+    res.send("OK");
+})
 
 
 
