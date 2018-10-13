c2a828431677894792c4b5f15e86c08f0dc2fd45
logiccosmic <logiccosmic@gmail.com>
2018-10-13 11:43:42 -0700

    Phase 3 COMPLETE

--- server.js
+++ server.js
@@ -5,9 +5,68 @@ var app = express();
 
 var PORT = 9001;
 
+var tables = [{
+    id: "fish",
+    person: "David",
+    email: "foo@foo.com",
+    phone: "1234567891"
+},
+{
+    id: "fish2",
+    person: "David",
+    email: "foo@foo.com",
+    phone: "1234567891"
+}];
+var waitlist = [{
+    id: "fish3",
+    person: "David",
+    email: "foo@foo.com",
+    phone: "1234567891"
+}];
+
+//
+// Table API
+//
+app.get("/api/tables", function(req, res){
+    return res.json(tables);
+})
+
+//
+// Waitlist API
+//
+app.get("/api/waitlist", function(req, res){
+    return res.json(waitlist);
+})
+
+
+//
+// Home Route
+//
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, "/home.html"));
+})
+
+//
+// Make Route
+//
+app.get("/make", function(req,res){
+    res.sendFile(path.join(__dirname, "/make.html"));
+})
+
+//
+// View Route
+//
+app.get("/view", function(req,res){
+    res.sendFile(path.join(__dirname, "/view.html"));
+})
 
 
 
-app.listen(PORT, function(){
+
+
+
+
+
+app.listen(PORT, function () {
     console.log("Hey this is running on a port formerly known as PORT: " + PORT)
 });
